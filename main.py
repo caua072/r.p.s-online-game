52ba9dd3d385d0474c7d7a25bb5e8f1b402f68a5
Cauã <cauabarbo@gmail.com>
2022-04-26 14:07:55 -0300

    class button

--- main.py
+++ main.py
@@ -1,12 +1,36 @@
 import pygame
 from network import Network
 from player import Player
+pygame.font.init()
 
-WIDTH = 500
-HEIGHT = 500
+WIDTH = 700
+HEIGHT = 700
 
 win = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption('Client')
+
+class Button:
+    def __init__(self, text, x, y, color):
+        self.text = text
+        self.x = x
+        self.y = y
+        self.color = color
+        self.width = 150
+        self.height = 100
+
+    def draw(self, win):
+        pygame.draw.rect(win, self.color, (self.x, self.y, self.width, self.height))
+        font = pygame.font.SysFont('arial', 40)
+        text = font.render(self.text, 1, (255, 255, 255))
+        win.blit(text, (self.x + round(self.width/2) - round(text.get_width()/2), self.y + round(self.height/2) - round(text.get_height()/2)))
+
+    def click(self, pos):
+        x1 = pos[0]
+        y1 = pos[1]
+        if self.x <= x1 <= self.x + self.width and self.y <= y1 <= self.y + self.height:
+            return True
+        else:
+            return False
 
 
 def redrawWindow(win, player, player2):
