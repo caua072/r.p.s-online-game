e343157c7ff975b25189ed8f0c49d068c5810993
Cauã <cauabarbo@gmail.com>
2022-04-26 16:59:29 -0300

    client

--- main.py
+++ main.py
@@ -33,32 +33,75 @@ class Button:
             return False
 
 
-def redrawWindow(win, player, player2):
+def redrawWindow(win, game, p):
 
-    win.fill((255, 255, 255))
-    player.draw(win)
-    player2.draw(win)
-    pygame.display.update()
+    win.fill((112, 109, 109))
+    
+    if not (game.connected()):
+        font = pygame.font.SysFont('arial', 80)
+        text = font.render('Wainting another connection', 1, (80, 110, 122), True)
+        win.blit(text, (WIDTH/2 - text.get_width()/2, HEIGHT/2 - text.get_height()/2))
+
+
+btns = [Button('R', 50, 500, (255, 0, 0)), Button('S', 250, 500, (0, 255, 0)), Button('P', 450, 500, (0, 0, 255))]
 
 def main():
 
     run = True
-    n = Network()
-    p = n.getP()
     clock = pygame.time.Clock()
-
+    n = Network()
+    player = int(n.getP)
+    print('Player: ', player)
 
     while run:
         clock.tick(60)
-        p2 = n.send(p)
+        try:
+            game = n.send('get')
+        except:
+            run = False
+            print('No game founded1')
+            break
 
-        for events in pygame.event.get():
-            if events.type == pygame.QUIT:
+        if game.bothWent():
+            redrawWindow()
+            pygame.time.delay(500)
+            try:
+                game = n.send('reset')
+            except:
+                run = False
+                print('No game founded2')
+                break
+            font = pygame.font.SysFont('arial', 90)
+            if (game.winner() == 1 and player == 1) or (game.winner() == 0 and player == 0):
+                text = font.render('You won!', 1, (0, 255, 0))
+            elif (game.winner() == 1 and player == 0) or (game.winner() == 0 and player == 1):
+                text = font.render('You lost', 1, (255, 0, 0))
+            else:
+                text = font.render('Tied game', 1, (0, 0, 255))
+
+            win.blit(text, (WIDTH/2 - text.get_width()/2, HEIGHT/2 - text.get_height()/2))
+            pygame.display.update()
+            pygame.time.delay(2000)
+
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
                 run = False
                 pygame.quit()
-                exit()
+            
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                pos = pygame.mouse.get_pos()
+                for btn in btns:
+                    if btn.click(pos) and game.connected():
+                        if player == 0:
+                            if not game.p1Went:
+                                n.send(btn.text)
+                        
+                        else:
+                            if not game.p2Went:
+                                n.send(btn.text)
         
-        p.move()
-        redrawWindow(win, p, p2)
+        redrawWindow(win, game, player)
+
+
 
 main()
